483d19eb7be1d65721e0e14abf02689f11353252
mi <island.ma@foxmail.com>
2017-03-17 13:33:44 +0800

    Adapter

---
+++ Design/Structure/Adapter/ChinesePlayer.php
@@ -0,0 +1,20 @@
+<?php
+namespace Design\Structure\Adapter;
+
+
+class ChinesePlayer
+{
+
+    public function pao()
+    {
+        return '跑';
+    }
+
+
+
+    public function tiao()
+    {
+        return '跳';
+    }
+
+}
---
+++ Design/Structure/Adapter/IPlayer.php
@@ -0,0 +1,16 @@
+<?php
+namespace Design\Structure\Adapter;
+
+/**
+ *
+ *
+ * Interface IPlayer
+ * @package Design\Structure\Adapter
+ */
+interface IPlayer
+{
+    public function run();
+
+    public function jump();
+
+}
